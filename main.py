226c602ce7d71eb96187a6327446b5be6e847ede
IvanJose0405 <ijosemendoza@unicesar.edu.co>
2023-05-09 14:47:27 -0500

    Pruebas con refactorizacion

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from typing import Union
+
 def Hello():
     return {"HELLO FASTAPI"}
 
 
 def Isprime(numero): 
-     return False
+     for n in range(2,numero):
+       if numero % n == 0:
+          return False  
+     return True
 
 
 def Fibonnacci(numero: int):
-     return 13
+     a=0
+     b=1
+     for n in range(0, numero):
+        c=a+b
+        a=b
+        b=c
+     return a
